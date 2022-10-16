41960166e6fc3387b17aa2f0522aafae1fcc3195
Benjamin Autin <ben@benjaminautin.com>
2022-10-15 19:39:03 -0500

    Here we go

--- legofinder.py
+++ legofinder.py
@@ -68,6 +68,7 @@ def runCheck(site, xpath):
         sameSets[setInfo.setid] = setInfo
       count = count + 1
   except urllib.error.HTTPError:
+    print(site)
     print('url lib error')
     return count
   except http.client.IncompleteRead:
