d7d7df1049ee51c02780326dce61b1d22609294f
Benjamin Autin <toast@thelab.benjaminautin.com>
2023-09-23 22:02:24 -0500

    Sql scripts

---
+++ sqlscripts/legotrack.sql
@@ -0,0 +1,40 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.1
+-- Dumped by pg_dump version 15.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: legotrack; Type: TABLE; Schema: public; Owner: lego
+--
+
+CREATE TABLE public.legotrack (
+    setid integer,
+    userid integer,
+    track boolean,
+    have boolean
+);
+
+
+ALTER TABLE public.legotrack OWNER TO lego;
+
+--
+-- PostgreSQL database dump complete
+--
+
